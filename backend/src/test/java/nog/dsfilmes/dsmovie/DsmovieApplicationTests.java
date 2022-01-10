63c0bd2d1a1f12aa6c379885c112950a6af3b15e
Deivison Nogueira <deivison.nogueira@live.com>
2022-01-10 08:46:03 -0400

    Project created

---
+++ backend/src/test/java/nog/dsfilmes/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package nog.dsfilmes.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
