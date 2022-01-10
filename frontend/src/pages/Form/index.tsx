35c8787068cea405192079aa9daa3c0bba024635
Deivison Nogueira <deivison.nogueira@live.com>
2022-01-10 10:31:18 -0400

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,8 @@
+function Form(){
+
+    return (
+        <h1>Page 2</h1>
+    );
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,8 @@
+function Listing(){
+
+    return (
+        <h1>Page</h1>
+    );
+}
+
+export default Listing;
