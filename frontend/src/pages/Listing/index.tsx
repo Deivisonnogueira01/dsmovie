5b35c11ca04a331accb97c090760797d1f628cb6
Deivison Nogueira <deivison.nogueira@live.com>
2022-01-14 09:15:31 -0400

    UseEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,19 +1,27 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Navbar/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-axios.get(`${BASE_URL}/movies`)
-.then(response => {
-    console.log(response.data);
-})
 
+const [pageNumber, setPageNumber] = useState(0);
 
+useEffect(() =>{
+    axios.get(`${BASE_URL}/movies`)
+    .then(response => {
+        const data = response.data as MoviePage;
+        console.log(data);
+        setPageNumber(data.number);
+    });
+}, []);
 
     return (
         <>
+      
             <Pagination />
 
             <div className="container">
